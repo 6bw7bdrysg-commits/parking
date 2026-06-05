28aa85bb8e1dad7aaf7d9d092f006fa5d243953b
6bw7bdrysg-commits <6bw7bdrysg@privaterelay.appleid.com>
2026-06-05 04:41:51 +0300

    Change database path to /tmp for write access
    Updated database URL to store the database in /tmp for write permissions.

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./parking.db"
+# Η βάση πρέπει να αποθηκεύεται στο /tmp για να έχει άδεια εγγραφής
+SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/parking.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
