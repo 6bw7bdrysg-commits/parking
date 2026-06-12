0b4108df3fb2f801816e0d571062cd9b6bddfb67
6bw7bdrysg-commits <6bw7bdrysg@privaterelay.appleid.com>
2026-06-12 17:59:43 +0300

    Add saved_lat and saved_lng to DBAppUser model
    Added new fields for saved latitude and longitude in DBAppUser.

--- models.py
+++ models.py
@@ -2,17 +2,18 @@ from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.sql import func
 from database import Base
 
-# Νέος πίνακας χρηστών για αποθήκευση Karma
 class DBAppUser(Base):
     __tablename__ = "app_users"
-    device_id = Column(String, primary_key=True, index=True)
+    device_id = Column(String, primary_key=True, index=True) # Εδώ αποθηκεύεται το Email
     karma = Column(Integer, default=0)
+    # ΝΕΑ ΠΕΔΙΑ: Αποθήκευση της Χρυσής Πινέζας στο Cloud
+    saved_lat = Column(Float, nullable=True)
+    saved_lng = Column(Float, nullable=True)
 
-# Νέος πίνακας θέσεων που δέχεται το string του device_id
 class DBParkingSpotV2(Base):
     __tablename__ = "parking_spots_v2"
     id = Column(Integer, primary_key=True, index=True)
-    device_id = Column(String, index=True) # Ποιος την ανέβασε
+    device_id = Column(String, index=True)
     latitude = Column(Float)
     longitude = Column(Float)
     minutes_until_free = Column(Integer)
