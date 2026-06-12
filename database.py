5a33f39f2e6c4658b75340a79a1f006e95ecce66
6bw7bdrysg-commits <6bw7bdrysg@privaterelay.appleid.com>
2026-06-12 15:48:21 +0300

    Update database file name in database.py
    Changed database file name from 'parkkarma_clean.db' to 'parking.db'.

--- database.py
+++ database.py
@@ -1,10 +1,9 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Η βάση πρέπει να αποθηκεύεται στο /tmp για να έχει άδεια εγγραφής
-SQLALCHEMY_DATABASE_URL = "sqlite:///./parkkarma_clean.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./parking.db"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
