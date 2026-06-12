ab649d3cb20598b2d47123614690689ca9d6eed2
6bw7bdrysg-commits <6bw7bdrysg@privaterelay.appleid.com>
2026-06-12 15:37:46 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Η βάση πρέπει να αποθηκεύεται στο /tmp για να έχει άδεια εγγραφής
-SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/parking.db"
-
+SQLALCHEMY_DATABASE_URL = "sqlite:///./parkkarma_clean.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
